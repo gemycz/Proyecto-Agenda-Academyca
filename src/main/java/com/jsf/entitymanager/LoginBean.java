a5b97d77add8701da5927d2e8be5a45211bdfe63
ESPE <ESPE@DESKTOP-FTI0RIG>
2019-12-10 20:03:21 -0500

    login

---
+++ src/main/java/com/jsf/entitymanager/LoginBean.java
@@ -0,0 +1,57 @@
+package com.jsf.entitymanager;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+@ManagedBean @SessionScoped
+public class LoginBean {
+
+	
+	private String password;
+	private String userName;
+	private static final String PERSISTENCE_UNIT_NAME = "PERSISTENCE";
+
+	public LoginBean() {}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/* Method To Check User's Authentication Credentials */
+	public String validateLoginCredentials() {
+		String validationResult = "";
+		EntityManager entityMgrObj = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME).createEntityManager();
+		Query queryObj = entityMgrObj.createQuery("SELECT u FROM UserEntityManager u WHERE u.login = :login AND u.password = :password");
+		queryObj.setParameter("login", userName);
+		queryObj.setParameter("password", password);
+		try {
+			UserEntityManager userResultSetObj = (UserEntityManager) queryObj.getSingleResult();				
+			if ((userResultSetObj != null) && (userName.equalsIgnoreCase(userResultSetObj.getLogin()) && password.equals(userResultSetObj.getPassword()))) {
+				validationResult ="indextareas.xhtml";
+				
+			} 
+		} catch(Exception exObj) {
+			validationResult = "login";
+			FacesContext.getCurrentInstance().addMessage("loginForm:loginName", new FacesMessage("Username Or Password Is Incorrect"));			
+		}
+		return validationResult;
+	}
+}
---
+++ src/main/java/com/jsf/entitymanager/UserEntityManager.java
@@ -0,0 +1,42 @@
+package com.jsf.entitymanager;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="login")
+public class UserEntityManager {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String login;
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
